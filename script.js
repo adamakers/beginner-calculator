724ed8180d1b003284142d66f27ff5b4ae4c8772
Adam Akers <adamrakers@gmail.com>
2022-03-14 21:17:43 -0600

    added functions for number handlers, operationhandler, and equal handler

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const buttons = document.querySelectorAll('.btn');
-const calcDisplay = document.querySelector('.calc-display');
+const calcDisplayElement = document.querySelector('.calc-display');
 
 
 let displayValue;
+let calculationHolder = {
+  prevValue: undefined,
+  operator: undefined
+}
 
 // MATHEMATICAL FUNCTIONS
 function operate(a, b, fn) {
-  return fn(a, b);
+  return fn(a,b);
 }
 
 function add(a, b) {
@@ -25,21 +29,62 @@ function divide(a, b) {
   return a / b;
 }
 
-// HANDLERS
-function buttonHandler(e) {
-  if (this.dataset.btntype === 'num') {
-    const num = this.textContent;
-    
-    calcDisplay.textContent =  calcDisplay.textContent += num;
-    displayValue = +calcDisplay.textContent;
-  }
+function numberHandler(e) {
+  const target = e.target;
+
+  if (target.textContent === '.' && displayValue.toString().includes('.')) return;
+  const num = target.textContent;
+  
+  calcDisplayElement.textContent =  calcDisplayElement.textContent += num;
+  displayValue = +calcDisplayElement.textContent;
 }
+
+function operatorHandler(e) {
+  //put number into calcHolder
+  calculationHolder.prevValue = displayValue;
+  // put operator into calcHolder
+  switch (e.target.textContent) {
+    case '+':
+      calculationHolder.operator = add;
+      break;
+    case '-':
+      calculationHolder.operator = subtract;
+      break;
+    case '*':
+      calculationHolder.operator = multiply;
+      break;
+    case '/':
+      calculationHolder.operator = divide;
+      break;
+  }
+
+  calcDisplayElement.textContent = '';
+}
+
 
 function equalHandler(e) {
-  if (this.classList.contains('equal')) {
-
-  }
+  displayValue = operate(calculationHolder.prevValue, displayValue, calculationHolder.operator);
+  calcDisplayElement.textContent = displayValue;
+  console.log(displayValue);
 }
+
+
+  // put the operation clicked into calcHolder
+  //clear display
+
+// HANDLERS
+function buttonHandler(e) {
+  if (this.dataset.btntype === 'equal') {
+    equalHandler(e);
+  } else if (this.dataset.btntype === 'num') {
+    numberHandler(e);
+  } else if (this.dataset.btntype === 'oper') {
+    operatorHandler(e);
+  }
+
+  
+}
+
 
 
 
