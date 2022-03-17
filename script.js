daa13ef300532fafe69911383198878f4112fe47
Adam Akers <adamrakers@gmail.com>
2022-03-16 21:41:59 -0600

    refactoring the whole project.  rewriting handlers

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-const buttons = document.querySelectorAll('.btn');
+//BUTTONS
+const numberButtons = document.querySelectorAll('.btn-num');
+const operatorButtons = document.querySelectorAll('.btn-oper');
+
 const calcDisplayElement = document.querySelector('.calc-display');
 
-
 let displayValue;
-let calculationHolder = {
+let calculator = {
+  currValue: '',
   prevValue: undefined,
   operator: undefined,
-  opPressed: false,
-  equalValue: false,
 }
 
 // MATHEMATICAL FUNCTIONS
-function operate(a, b, symbol) {
-
-  switch (symbol) {
-    case '+':
-      return add(a, b);
-    case '-':
-      return subtract(a, b);
-    case '*':
-      return multiply(a, b);
-    case '/':
-      return divide(a, b);
-  }
-}
 
 function add(a, b) {
   return a + b;
@@ -41,84 +29,59 @@ function divide(a, b) {
   return a / b;
 }
 
-//HELPERS
-function divideByZeroError() {
-  // if ()
+function operate(a, b, symbol) {
+
+  switch (symbol) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+  }
 }
+
+//HELPERS
 
 // HANDLERS
 function clearHandler() {
   displayValue = 0;
   calcDisplayElement.textContent = '';
-  calculationHolder.operator = undefined;
-  calculationHolder.prevValue = undefined;
+  calculator.operator = undefined;
+  calculator.prevValue = undefined;
 }
 
 //Handles the number buttons on the calculator
 function numberHandler(e) {
+
   const target = e.target;
+  const targetValue = target.textContent;
 
-  if (calculationHolder.finalValue) {
-    clearHandler();
-    calculationHolder.finalValue = false;
-  }
+  calculator.currValue += targetValue;
 
-  if (calculationHolder.opPressed) {
-    calculationHolder.opPressed = false;
-    displayValue = '';
-    calcDisplayElement.textContent = displayValue;
-  }
-
-  if (target.textContent === '.' && displayValue.toString().includes('.')) return;
-
-  const num = target.textContent;
-
-  calcDisplayElement.textContent = calcDisplayElement.textContent += num;
-  displayValue = +calcDisplayElement.textContent;
+  calcDisplayElement.textContent = calculator.currValue;
 }
 
 //Handles the operator buttons
 function operatorHandler(e) {
-  const operSymbol = e.target.textContent;
 
-
-
-  if (calculationHolder.operator) {
-    displayValue = operate(calculationHolder.prevValue, displayValue, calculationHolder.operator);
-    calcDisplayElement.textContent = displayValue;
-  }
-
-  calculationHolder.prevValue = displayValue;
-  calculationHolder.operator = operSymbol;
-  calculationHolder.opPressed = true;
 }
 
 //Handles the equal button
 function equalHandler(e) {
-  displayValue = operate(calculationHolder.prevValue, displayValue, calculationHolder.operator);
-  calculationHolder.prevValue = displayValue;
-  calcDisplayElement.textContent = displayValue;
-  displayValue = 0;
-  calculationHolder.finalValue = true;
-}
 
-//The core button handler.  The event delegator
-function buttonHandler(e) {
-  if (this.dataset.btntype === 'equal') {
-    equalHandler(e);
-  } else if (this.dataset.btntype === 'num') {
-    numberHandler(e);
-  } else if (this.dataset.btntype === 'oper') {
-    operatorHandler(e);
-  } else if (this.dataset.btntype === 'clear') {
-    clearHandler();
-  }
 }
-
 
 // EVENT LISTENERS
-buttons.forEach(button => {
-  button.addEventListener('click', buttonHandler)
+
+numberButtons.forEach(btn => {
+  btn.addEventListener('click', numberHandler);
+});
+
+operatorButtons.forEach(btn => {
+  btn.addEventListener('click', operatorHandler);
 });
 
 
